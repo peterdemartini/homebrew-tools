d2db54f1b7a8b310db4e0bea802e699dde25adc4
Peter DeMartini <thepeterdemartini@gmail.com>
2016-08-19 07:13:12 -0700

    vimux v2.0.1

--- Formula/vimux.rb
+++ Formula/vimux.rb
@@ -1,9 +1,9 @@
 class Vimux < Formula
-  VERSION = "2.0.0"
+  VERSION = "2.0.1"
 
   desc "Tmux project session with vim"
   url "https://github.com/peterdemartini/dev-tools-vimux/archive/v#{VERSION}.tar.gz"
-  sha256 "9420e148163efd7c711037a7c48925fbc1c166580b68bc6b6990bda3d2635069"
+  sha256 "f1fce6b8cfbc87597fa34aadf042e6d02af01b06386248cb6d222e603b37fd3e"
 
   depends_on "tmux"
 
