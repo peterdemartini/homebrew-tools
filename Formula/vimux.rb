88938f0d7cb4b03821128f2ef0f283b20c4e7825
Peter DeMartini <thepeterdemartini@gmail.com>
2016-09-12 09:52:40 -0700

    updated vimux to v2.1.2

--- Formula/vimux.rb
+++ Formula/vimux.rb
@@ -1,9 +1,9 @@
 class Vimux < Formula
-  VERSION = "2.1.1"
+  VERSION = "2.1.2"
 
   desc "Tmux project session with vim"
   url "https://github.com/peterdemartini/dev-tools-vimux/archive/v#{VERSION}.tar.gz"
-  sha256 "7d1dc3fcebc2906b561c72e42229f6c5d3e04d46c48aa06e8c36a31d3437d185"
+  sha256 "ff007dc5efcb0c732a9cdde393e8c10cbca5826417d5b58f1e0bc87c21a5f839"
 
   depends_on "tmux"
 
